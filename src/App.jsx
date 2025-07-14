50ae2cb6de747fbe8eed382a86b4d857301363cf
bakryelzzubair <bakryelzzubair@gmail.com>
2025-07-14 18:05:38 +0300

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,84 +1,107 @@
-import omelette from "../public/assets/images/image-omelette.jpeg";
-function App() {
+// import omelette from "../public/assets/images/image-omelette.jpeg";
+import React from "react";
+// import omelette from "../public/assets/images/image-omelette.jpeg";
+
+const App = () => {
   return (
-    <div className=" container mx-auto px-5 py-5 md:px-6 lg:px-32 xl:px-60 2xl:px-100">
-      <div className="bg-white p-10">
-        <img src={omelette} alt="" className="w-full rounded-lg" />
-        <h1 className="font-youngSerif-400 text-primary-Stone-900 capitalize font-semibold pt-8 pb-4 text-2xl sm:text-4xl md:text-5xl">
-          simple omelette recipe
-        </h1>
-        <p className="text-justify sm:text-xl md:text-2xl lg:text-3xl">
-          An easy and quick dish, perfect for any meal. This classic omelette
-          combines beaten eggs cooked to perfection, optionally filled with your
-          choice of cheese, vegetables, or meats.
-        </p>
-        <div className="bg-primary-Rose-50 p-6 my-4 rounded-xl ">
-          <h3 className="text-primary-Rose-800 font-outfit-700 mb sm:text-xl md:text-2xl lg:text-3xl">
-            Preparation time
-          </h3>
-          <ul className="sm:text-xl md:text-2xl lg:text-3xl">
-            {properations.map((item) => (
-              <li key={item.id}>
-                <span className=" text-primary-Stone-600 font-outfit-700">{`${item.title} : `}</span>{" "}
-                {item.desc}
-              </li>
-            ))}
-          </ul>
-        </div>
-        {/* ingredients */}
-        <div className="my-4 border-b-[0.1px] py-4">
-          <h2 className="sm:text-2xl md:text-4xl my-4">Ingredients</h2>
-          <ul className="sm:text-xl md:text-2xl lg:text-3xl">
-            {ingredients.map((item) => (
-              <li key={item.id}>{item.desc}</li>
-            ))}
-          </ul>
-        </div>
-        {/* instructions */}
-        <div className="my-4 border-b-[0.1px] py-4">
-          <h2 className="sm:text-2xl md:text-4xl my-4">Instructions</h2>
-          <ol
-            className=" space-y-2 sm:text-xl md:text-2xl lg:text-3xl"
-            style={{ marginLeft: "15px" }}
-          >
-            {instructions.map((item) => (
-              <li key={item.id} style={{ lineHeight: 1.5 }}>
-                <span className="mr-2 text-primary-Brown-800 font-outfit-700 text-xl">{`${item.id}.`}</span>
-                <span className="mr-2 text-primary-Stone-600 font-outfit-700">{`${item.title}: `}</span>
-                {item.desc}
-              </li>
-            ))}
-          </ol>
-        </div>
-        {/* neutrition */}
-        <div className="my-4 py-4">
-          <h2 className="sm:text-2xl md:text-4xl my-4">Neutrition</h2>
-          <p className="sm:text-xl md:text-2xl lg:text-3xl">
-            The table below show neutrintonsl values per servings without the
-            addintonal fillings.
-          </p>
-          <table className="w-full my-6 flex flex-col gap-2 sm:text-xl md:text-2xl lg:text-3xl">
-            {neutrition.map((item) => (
-              <tr
-                key={item.id}
-                className="border-b border-primary-stone-150 grid grid-cols-2 gap-6"
-              >
-                <td className="pl-18">{item.title}</td>
-                <td className="text-primary-Brown-800 font-outfit-700">
-                  {item.value}
-                </td>
-              </tr>
-            ))}
-          </table>
+    <div className="container mx-auto sm:px-4 sm:pt-4 md:px-8 lg:px-10 xl:px-32 2xl:px-60 md:pt-8 lg:pt-10 md:mt-4">
+      <div className="sm:p-4 sm:rounded-xl bg-white">
+        {/* header */}
+        <header className="pb-4">
+          <img
+            src="../public/assets/images/image-omelette.jpeg"
+            alt="omelette with ages and meate"
+            className=" sm:rounded-md"
+          />
+          <div className="px-4 sm:px-0">
+            <h1 className=" my-4 text-primary-Stone-900 font-youngSerif-400 text-2xl sm:text-3xl md:text-4xl font-black">
+              Simple Omelette Recipe
+            </h1>
+            <p>
+              An easy and quick dish, perfect for any meal. This classic
+              omelette combines beaten eggs cooked to perfection, optionally
+              filled with your choice of cheese, vegetables, or meats.
+            </p>
+          </div>
+        </header>
+
+        <div className="px-4 sm:px-0">
+          {/* preperation time section */}
+          <section className="bg-primary-Rose-50 p-4 sm:p-6 md:p-8 rounded-md">
+            <h2 className="text-primary-Rose-800 font-outfit text-md sm:text-xl md:text-2xl font-bold md:font-black mb-4">
+              Preparation time
+            </h2>
+            <ul className=" ml-8 space-y-1">
+              {preperations.map((item) => (
+                <li key={item.id} className="flex items-center">
+                  <span className="w-2 h-2 rounded-full bg-primary-Brown-800 inline-block mr-4"></span>
+                  <span className="text-primary-Stone-600 font-bold sm:font-black mr-1">
+                    {`${item.title}:`}{" "}
+                  </span>
+                  {item.desc}
+                </li>
+              ))}
+            </ul>
+          </section>
+
+          {/* ingredients section */}
+          <section className="py-8">
+            <h2 className="text-primary-Brown-800 font-outfit text-md sm:text-xl md:text-2xl font-bold md:font-black mb-4">
+              Ingredients
+            </h2>
+            <ul className=" ml-8 space-y-1">
+              {ingredients.map((item) => (
+                <li key={item.id} className="flex items-center">
+                  <span className="text-primary-Brown-800 inline-block mr-4 font-bold sm:font-black">{`${item.id}. `}</span>
+                  {item.desc}
+                </li>
+              ))}
+            </ul>
+          </section>
+
+          {/* instructions section */}
+          <section className="py-8">
+            <h2 className="text-primary-Brown-800 font-outfit text-md sm:text-xl md:text-2xl font-bold md:font-black mb-4">
+              Instructions
+            </h2>
+            <ol className=" ml-8 space-y-1">
+              {instructions.map((item) => (
+                <li key={item.id} className="">
+                  <span className="text-primary-Brown-800 mr-4 font-bold sm:font-black">{`${item.id}. `}</span>
+                  <span className="text-primary-Stone-600 font-bold sm:font-black mr-1">{`${item.title}: `}</span>
+                  {item.desc}
+                </li>
+              ))}
+            </ol>
+          </section>
+
+          {/* neutrition section */}
+          <section>
+            <h2 className="text-primary-Brown-800 font-outfit text-md sm:text-xl md:text-2xl font-bold md:font-black mb-4">
+              Neutrition
+            </h2>
+            <p>
+              The table below shows nutritional values per serving without the
+              additional fillings.
+            </p>
+            <table>
+              {neutrition.map((item) => (
+                <tr key={item.id}>
+                  <td>{item.title}</td>
+                  <td>{item.value}</td>
+                </tr>
+              ))}
+            </table>
+          </section>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default App;
 
-const properations = [
+const preperations = [
   { id: 1, title: "Total", desc: "Approximately 10 minutes" },
   { id: 2, title: "Preparation", desc: "5 minutes" },
   { id: 3, title: "Cooking", desc: "5 minutes" },
@@ -90,7 +113,7 @@ const ingredients = [
   { id: 3, desc: "Pepper, to taste" },
   { id: 4, desc: "1 tablespoon of butter or oil" },
   {
-    id: 4,
+    id: 5,
     desc: "Optional fillings: cheese, diced vegetables, cooked meats, herbs",
   },
 ];
@@ -129,8 +152,8 @@ const instructions = [
 ];
 
 const neutrition = [
-  {id: 1, title: "Calories", value: "277Kcal"},
-  {id: 2, title: "Carbs", value: "0g"},
-  {id: 3, title: "Proten", value: "20g"},
-  {id: 4, title: "Fat", value: "22g"},
-]
+  { id: 1, title: "Calories", value: "277Kcal" },
+  { id: 2, title: "Carbs", value: "0g" },
+  { id: 3, title: "Proten", value: "20g" },
+  { id: 4, title: "Fat", value: "22g" },
+];
